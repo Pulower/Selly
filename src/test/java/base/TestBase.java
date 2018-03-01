d220724b2b877ddffb995eea35eb678f074dc566
Pulower <w.dzik111@gmail.com>
2018-03-01 12:17:12 +0100

    Added methods for creating logs and srceenshots

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,28 +3,54 @@ package base;
 import Driver.DriverManager;
 import Driver.DriverManagerFactory;
 import Driver.DriverType;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 public class TestBase {
 
     protected WebDriver driver;
+    private String address = "https://demo.selly.pl/";
     private DriverManager driverManager;
+    private static int screenId = 0;
+
+    private static void printBrowserLogs(WebDriver driver){
+        System.out.println("Browser logs");
+        LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
+        for(LogEntry entry : logEntries){
+            System.out.println("Timestamp: " + new Date(entry.getTimestamp()) +
+                    "\nLevel: " + entry.getLevel() +
+                    "\nMessage: " + entry.getMessage());
+        }
+    }
+
+    private void takeScreenshot() throws IOException {
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("resources/screenshots/" + Integer.toString(screenId) + ".jpg"));
+        screenId++;
+    }
 
     @BeforeTest
-    public void setUpType(){
-        driverManager = DriverManagerFactory.getManager(DriverType.CHROME);
-    }
-
-    @BeforeMethod
     public void setUpDriver(){
+        driverManager = DriverManagerFactory.getManager(DriverType.CHROME);
         driver = driverManager.getDriver();
+        driver.get(address);
     }
 
-    @AfterMethod
-    public void tearDown(){
+    @AfterTest
+    public void tearDown() throws IOException {
+        takeScreenshot();
+        printBrowserLogs(driver);
         driverManager.quitDriver();
     }
 }
